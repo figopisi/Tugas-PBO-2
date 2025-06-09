12de7eb3e3b4aa3e8abfea81918482852ec1f3ec
ritamutiara <ritamutiara1604@gmail.com>
2025-06-09 21:24:53 +0800

    villa model

--- src/model/Villa.java
+++ src/model/Villa.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Villa {
+    private int id;
+    private String name;
+    private String description;
+    private String address;
+
+    public Villa(int id, String name, String description, String address) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.address = address;
+    }
+
+    public Villa(String name, String description, String address) {
+        this(0, name, description, address);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
