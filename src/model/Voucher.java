35c64e10a8b30fab07b8be3c9c4d266bc967f354
LidyaSunu <plidyaps@gmail.com>
2025-06-09 21:47:43 +0800

    voucher model

--- src/model/Voucher.java
+++ src/model/Voucher.java
@@ -1,4 +1,51 @@
 package model;
 
 public class Voucher {
+    private int id;
+    private String code;
+    private String description;
+    private double discount;
+    private String startDate;
+    private String endDate;
+
+  public Voucher(int id, String code, String description, double discount, String startDate, String endDate) {
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.discount = discount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    
+    public Voucher(String code, String description, double discount, String startDate, String endDate) {
+        this(0, code, description, discount, startDate, endDate);
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public double getDiscount() {
+        return discount;
+    }
+    
+    public String getStartDate() {
+        return startDate;
+    }
+    
+    public String getEndDate() {
+        return endDate;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
 }
