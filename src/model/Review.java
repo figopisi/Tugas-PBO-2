ea48efafe6502300089f8709985dded0a8b5147d
ritamutiara <ritamutiara1604@gmail.com>
2025-06-09 21:43:32 +0800

    Review model

--- src/model/Review.java
+++ src/model/Review.java
@@ -1,4 +1,50 @@
 package model;
 
 public class Review {
+    private int bookingId;
+    private int star;
+    private String title;
+    private String content;
+
+    public Review() {}
+
+    public Review(int bookingId, int star, String title, String content) {
+        this.bookingId = bookingId;
+        this.star = star;
+        this.title = title;
+        this.content = content;
+    }
+
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
