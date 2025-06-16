a3f8d85e0ee021f68fa4c2f27100de6e8518d2a5
mrdawn656 <fajarharryputra@gmail.com>
2025-06-16 22:38:07 +0800

    up VillaReviewhandler.java in controller\villaController package

---
+++ src/Controller/VillaController/VillaReviewHandler.java
@@ -0,0 +1,35 @@
+package Controller.VillaController;
+
+import com.sun.net.httpserver.HttpExchange;
+import util.Response;
+import service.VillaService.VillaReviewService;
+import util.Exception.ApiException;
+import web.Server;
+
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+public class VillaReviewHandler {
+
+    public static void handle(HttpExchange exchange, String method, String path) throws Exception {
+        Response res = new Response(exchange);
+
+        try {
+            if (method.equals("GET") && path.matches("/villas/\\d+/reviews")) {
+                int villaId = Integer.parseInt(path.split("/")[2]);
+                VillaReviewService.indexByVilla(villaId, res);
+            } else {
+                res.setBody(Server.jsonMap(Map.of("status", 405, "message", "Method Not Allowed")));
+                res.send(HttpURLConnection.HTTP_BAD_METHOD);
+            }
+
+        } catch (ApiException e) {
+            res.setBody(Server.jsonMap(Map.of("status", e.getStatus(), "message", e.getMessage())));
+            res.send(e.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setBody(Server.jsonMap(Map.of("status", 500, "message", "Internal Server Error")));
+            res.send(HttpURLConnection.HTTP_INTERNAL_ERROR);
+        }
+    }
+}
