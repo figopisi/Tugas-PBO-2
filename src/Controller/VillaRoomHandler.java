0f3649dc14e6b2040bae1f50e8f3b806b0916b2b
mrdawn656 <fajarharryputra@gmail.com>
2025-06-16 22:16:29 +0800

    create controller for villa room endpoint

---
+++ src/Controller/VillaRoomHandler.java
@@ -0,0 +1,47 @@
+package Controller;
+
+import com.sun.net.httpserver.HttpExchange;
+import util.Request;
+import util.Response;
+import util.Exception.ApiException;
+import service.VillaService.VillaRoomService;
+import web.Server;
+
+import java.net.HttpURLConnection;
+import java.util.Map;
+
+public class VillaRoomHandler {
+
+    public static void handle(HttpExchange exchange, String method, String path) throws Exception {
+        Request req = new Request(exchange);
+        Response res = new Response(exchange);
+
+        try {
+            if (method.equals("GET") && path.equals("/room_types")) {
+                VillaRoomService.index(res);
+            } else if (method.equals("GET") && path.matches("/room_types/\\d+")) {
+                int id = Integer.parseInt(path.split("/")[2]);
+                VillaRoomService.show(id, res);
+            } else if (method.equals("POST") && path.equals("/room_types")) {
+                VillaRoomService.create(req, res);
+            } else if (method.equals("PUT") && path.matches("/room_types/\\d+")) {
+                int id = Integer.parseInt(path.split("/")[2]);
+                VillaRoomService.update(id, req, res);
+            } else if (method.equals("DELETE") && path.matches("/room_types/\\d+")) {
+                int id = Integer.parseInt(path.split("/")[2]);
+                VillaRoomService.destroy(id, res);
+            } else {
+                res.setBody(Server.jsonMap(Map.of("status", 405, "message", "Method Not Allowed")));
+                res.send(HttpURLConnection.HTTP_BAD_METHOD);
+            }
+
+        } catch (ApiException e) {
+            res.setBody(Server.jsonMap(Map.of("status", e.getStatus(), "message", e.getMessage())));
+            res.send(e.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setBody(Server.jsonMap(Map.of("status", 500, "message", "Internal Server Error")));
+            res.send(HttpURLConnection.HTTP_INTERNAL_ERROR);
+        }
+    }
+}
