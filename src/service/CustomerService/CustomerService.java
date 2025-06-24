588da29940808bd2a72c96a2281ae9ef0824e70b
LidyaSunu <plidyaps@gmail.com>
2025-06-24 10:04:18 +0800

    CustomerService.java

--- src/service/CustomerService/CustomerService.java
+++ src/service/CustomerService/CustomerService.java
@@ -3,9 +3,9 @@ package service.CustomerService;
 import config.Database;
 import model.Customer;
 import util.Request;
+import util.Response;
 import util.Exception.ApiException;
 import util.validator.CustomerValidator;
-import util.Response;
 import web.Server;
 
 import java.net.HttpURLConnection;
@@ -56,6 +56,57 @@ public class CustomerService {
             );
 
             res.setBody(Server.jsonMap(Map.of("status", 200, "data", customer)));
+            res.send(HttpURLConnection.HTTP_OK);
+        }
+    }
+
+    public static void create(Request req, Response res) throws Exception {
+        Map<String, Object> body = req.getJSON();
+        CustomerValidator.validateInput(body);
+
+        try (Connection connection = Database.getConnection()) {
+            String query = "INSERT INTO customers(name, email, phone) VALUES (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, (String) body.get("name"));
+            statement.setString(2, (String) body.get("email"));
+            statement.setString(3, (String) body.get("phone"));
+            statement.executeUpdate();
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            generatedKeys.next();
+
+            res.setBody(Server.jsonMap(Map.of(
+                    "status", 201,
+                    "id", generatedKeys.getInt(1),
+                    "message", "Customer berhasil dibuat (registrasi berhasil)"
+            )));
+            res.send(HttpURLConnection.HTTP_CREATED);
+        }
+    }
+
+    public static void update(int id, Request req, Response res) throws Exception {
+        Map<String, Object> body = req.getJSON();
+        CustomerValidator.validateInput(body);
+
+        try (Connection connection = Database.getConnection()) {
+            String checkQuery = "SELECT id FROM customers WHERE id = ?";
+            PreparedStatement checkStatement = connection.prepareStatement(checkQuery);
+            checkStatement.setInt(1, id);
+            ResultSet rs = checkStatement.executeQuery();
+
+            if (!rs.next()) {
+                throw new ApiException.NotFoundApiException("Customer dengan id " + id + " tidak ditemukan");
+            }
+
+            String updateQuery = "UPDATE customers SET name = ?, email = ?, phone = ? WHERE id = ?";
+            PreparedStatement updateStatement = connection.prepareStatement(updateQuery);
+            updateStatement.setString(1, (String) body.get("name"));
+            updateStatement.setString(2, (String) body.get("email"));
+            updateStatement.setString(3, (String) body.get("phone"));
+            updateStatement.setInt(4, id);
+            updateStatement.executeUpdate();
+
+            res.setBody(Server.jsonMap(Map.of("status", 200, "message", "Data customer berhasil diubah")));
             res.send(HttpURLConnection.HTTP_OK);
         }
     }
