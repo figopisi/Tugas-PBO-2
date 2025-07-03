8a752cd19709bc97caba599d60f6b2394293f510
Figo Laksmana <figodh117@gmail.com>
2025-07-03 17:33:58 +0800

    Fix: Correct JSON parsing in Request.getJSON to handle non-null jsonBody

--- src/util/Request.java
+++ src/util/Request.java
@@ -9,7 +9,6 @@ import util.Exception.GeneralException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
