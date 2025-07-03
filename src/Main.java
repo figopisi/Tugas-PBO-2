e9a457b3ef082462df940f3ee861e258f76910ef
Figo Laksmana <figodh117@gmail.com>
2025-07-03 17:34:27 +0800

    Adding notes for local host

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import web.Route;
 
 public class Main {
     public static final String API_KEY = "S3CR3T_API_KEY_123";
-
+    // http://localhost:8080/villas// Link
     public static void main(String[] args) throws Exception {
         int port = 8080;
         if (args.length == 1) port = Integer.parseInt(args[0]);
